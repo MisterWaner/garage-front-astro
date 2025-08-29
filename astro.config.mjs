d7e2f5703bf806160f660574bbd10c0d14ae2b78
Erwan Soulas <erwan.soulas@gmail.com>
2025-08-29 22:08:13 +0200

    4th commit: --PREPARE project for fetching data from SanityCMS.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,16 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
+import sanity from '@sanity/astro';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
-  integrations: [react()]
+  integrations: [react(), sanity({
+    projectId: 'yourProjectId',
+    dataset: 'production',
+    useCdn: false
+  })]
 });
